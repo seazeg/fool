0a848137a9b95c41ffe07fbe28239c477373b2e1
Evan.G <seazeg@126.com>
2020-05-22 19:46:04 +0800

    default Commit Message [Evan.G]

--- src/web/components/layout/layout-sidetools.vue
+++ src/web/components/layout/layout-sidetools.vue
@@ -2,70 +2,34 @@
   <div class="layout-sidetools">
     <span class="operate iconfont icon-youshuangjiantou"></span>
     <div class="group">
-      <div class="box" v-for="item in navInfo">
-        <a class="iconfont" :class="[{'checked':item.flag},item.icon]" @click="to(item)">
+      <div class="box" v-for="item in navInfo" :class="{'checked':$route.name==item.meta.lname}">
+        <a class="iconfont" :class="item.meta.icon" @click="goPage(item)">
         </a>
       </div>
     </div>
   </div>
 </template>
 <script>
+  import {
+    navInfo
+  } from '../../router.js'
+  console.log(navInfo);
   export default {
     name: "layout-sidetools",
     data() {
       return {
-        navInfo: [{
-          "name": "按钮",
-          "eName": "button",
-          "icon": "icon-anniu",
-          "href": "/button",
-          "flag": false
-        }, {
-          "name": "文本框",
-          "eName": "input",
-          "icon": "icon-danhangshurukuang",
-          "href": "/input",
-          "flag": false
-        },{
-          "name": "单选",
-          "eName": "button",
-          "icon": "icon-danxuanyixuan",
-          "href": "/radio",
-          "flag": false
-        },{
-          "name": "多选",
-          "eName": "button",
-          "icon": "icon-duoxuanyixuan",
-          "href": "/checkbox",
-          "flag": false
-        },{
-          "name": "下拉框",
-          "eName": "button",
-          "icon": "icon-xialakuangbiaodan",
-          "href": "/selector",
-          "flag": false
-        },{
-          "name": "下拉框",
-          "eName": "button",
-          "icon": "icon-yulan",
-          "href": "/compressIMG",
-          "flag": false
-        }]
+        navInfo: navInfo
       }
     },
     methods: {
-      to(obj) {
-        this.$router.push({
-          path: obj.href,
-          query: {
-            type: obj.eName
-          }
+      goPage(obj) {
+        this.$router.replace({
+          path: obj.path,
         })
-        for (var i in this.navInfo) {
-          this.navInfo[i].flag = false;
-        }
-        obj.flag = true;
       }
+    },
+    mounted() {
+
     }
   }
 </script>
--- src/web/router.js
+++ src/web/router.js
@@ -3,6 +3,73 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+export const navInfo = [{
+  path: '/button',
+  name: 'button',
+  component: () => import( /* webpackChunkName: "button" */ './views/Button/button.vue'),
+  meta: {
+    lname: '按钮',
+    icon: "icon-anniu",
+  }
+}, {
+  path: '/selector',
+  name: 'selector',
+  component: () => import( /* webpackChunkName: "button" */ './views/Selector/selector.vue'),
+  meta: {
+    lname: '下拉框',
+    icon: "icon-xialakuangbiaodan",
+  }
+}, {
+  path: '/input',
+  name: 'input',
+  component: () => import( /* webpackChunkName: "button" */ './views/Input/input.vue'),
+  meta: {
+    lname: '输入框',
+    icon: "icon-duoxuanyixuan",
+  }
+}, {
+  path: '/checkbox',
+  name: 'checkbox',
+  component: () => import( /* webpackChunkName: "button" */ './views/Checkbox/checkbox.vue'),
+  meta: {
+    lname: '多选框',
+    icon: "icon-danhangshurukuang",
+  }
+}, {
+  path: '/radio',
+  name: 'radio',
+  component: () => import( /* webpackChunkName: "button" */ './views/Radio/radio.vue'),
+  meta: {
+    lname: '单选框',
+    icon: "icon-danxuanyixuan",
+  }
+}, {
+  path: '/table',
+  name: 'table',
+  component: () => import( /* webpackChunkName: "button" */ './views/Table/table.vue'),
+  meta: {
+    lname: '表格',
+    icon: "icon-duoxuanyixuan",
+  }
+}, {
+  path: '/pager',
+  name: 'pager',
+  component: () => import( /* webpackChunkName: "button" */ './views/Pager/pager.vue'),
+  meta: {
+    lname: '分页器',
+    icon: "icon-duoxuanyixuan",
+  }
+}, {
+  path: '/form',
+  name: 'form',
+  component: () => import( /* webpackChunkName: "button" */ './views/Pager/pager.vue'),
+  meta: {
+    lname: '表单',
+    icon: "icon-duoxuanyixuan",
+  }
+}]
+
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -15,15 +82,6 @@ export default new Router({
       name: 'initialPage',
       component: () => import( /* webpackChunkName: "button" */ './views/Button/button.vue')
     },
-    {
-      path: '/button',
-      name: 'button',
-      component: () => import( /* webpackChunkName: "button" */ './views/Button/button.vue')
-    },
-    {
-      path: '/selector',
-      name: 'selector',
-      component: () => import( /* webpackChunkName: "button" */ './views/Selector/selector.vue')
-    }
+    ...navInfo
   ]
 })

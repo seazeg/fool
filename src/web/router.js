49a3a19d2b6cabeaa38c33bd9024257ee725e243
Evan.G <seazeg@126.com>
2020-10-15 11:35:45 +0800

    default Commit Message [Evan.G]

--- src/web/router.js
+++ src/web/router.js
@@ -1,3 +1,9 @@
+/*
+ * @Author       : Evan.G
+ * @Date         : 2020-05-18 15:06:51
+ * @LastEditTime : 2020-10-14 18:11:10
+ * @Description  : 
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
 
@@ -7,6 +13,10 @@ export const navInfo = [{
   path: '/editor',
   name: 'editor',
   component: () => import( /* webpackChunkName: "button" */ './components/layout/layout-editor.vue'),
+},{
+  path: '/demo',
+  name: 'demo',
+  component: () => import( /* webpackChunkName: "demo" */ './demo.vue'),
 }]
 
 

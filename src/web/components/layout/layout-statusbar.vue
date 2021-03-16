2e0a07c4ce5c01105215e68c129c648e0b7ac69b
Evan.G <seazeg@126.com>
2021-03-16 15:06:36 +0800

    default Commit Message [Evan.G]

--- src/web/components/layout/layout-statusbar.vue
+++ src/web/components/layout/layout-statusbar.vue
@@ -1,23 +1,25 @@
 <!--
  * @Author       : Evan.G
  * @Date         : 2021-03-16 11:18:27
- * @LastEditTime : 2021-03-16 14:41:01
+ * @LastEditTime : 2021-03-16 15:06:05
  * @Description  : 
 -->
 <template>
     <div class="layout-statusbar" v-if="ele.name">
         <div class="left box">
-            <span class="name">组件信息: {{ ele.name }} | {{ ele.label }}</span>
+            <span class="name">组件信息: {{ ele.name }} - {{ ele.label }}</span>
         </div>
         <div class="right box">
             <span class="status"
                 >组件状态:
                 {{ ele.zoomParams.draggable ? "未锁定" : "已锁定" }}</span
             >
+            <b>|</b>
             <span class="status"
                 >比例缩放:
                 {{ ele.zoomParams.lockAspectRatio ? "已开启" : "已关闭" }}</span
             >
+            <b>|</b>
             <span class="location"
                 >X:{{ ele.zoomParams.x }}, Y:{{ ele.zoomParams.y }}</span
             >

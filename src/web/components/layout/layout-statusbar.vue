f7e25c4d3873bd57cbe8f1e40cab7fc091fb7dd4
Evan.G <seazeg@126.com>
2021-03-25 15:23:16 +0800

    2

--- src/web/components/layout/layout-statusbar.vue
+++ src/web/components/layout/layout-statusbar.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author       : Evan.G
  * @Date         : 2021-03-16 11:18:27
- * @LastEditTime : 2021-03-16 15:06:05
+ * @LastEditTime : 2021-03-24 11:41:15
  * @Description  : 
 -->
 <template>
@@ -10,12 +10,12 @@
             <span class="name">组件信息: {{ ele.name }} - {{ ele.label }}</span>
         </div>
         <div class="right box">
-            <span class="status"
+            <span class="status" @click="statusToggle"
                 >组件状态:
                 {{ ele.zoomParams.draggable ? "未锁定" : "已锁定" }}</span
             >
             <b>|</b>
-            <span class="status"
+            <span class="status" @click="lockToggle"
                 >比例缩放:
                 {{ ele.zoomParams.lockAspectRatio ? "已开启" : "已关闭" }}</span
             >
@@ -32,6 +32,15 @@ export default {
     name: "layout-statusbar",
     props: {
         ele: [Array, Object],
+    },
+    methods: {
+        statusToggle() {
+            this.ele.zoomParams.draggable = !this.ele.zoomParams.draggable;
+        },
+        lockToggle() {
+            this.ele.zoomParams.lockAspectRatio = !this.ele.zoomParams
+                .lockAspectRatio;
+        },
     },
 };
 </script>
